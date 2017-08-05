cc0ea3ec8ed9c781df8b221364b48efde7777280
Nikhil <niksvij@hotmail.com>
2017-08-05 10:41:28 +0530

    Add Constants file and Register Servlet

--- src/main/java/models/UserModel.java
+++ src/main/java/models/UserModel.java
@@ -2,6 +2,7 @@ package models;
 
 import com.google.gson.JsonObject;
 import entities.*;
+import utils.Constants;
 import utils.GsonMessageBodyHandler;
 
 import javax.jws.soap.SOAPBinding;
@@ -16,38 +17,39 @@ import java.util.List;
  */
 public class UserModel {
     private Client client = ClientBuilder.newClient().register(GsonMessageBodyHandler.class);
-    private String URL="http://localhost:8080/";
+    private String URL = Constants.URL;
+
     public User getUser(long id) {
-        User user  = client
-                .target(URL+"user/" + id)
+        User user = client
+                .target(URL + Constants.URL_USER + id)
                 .request(MediaType.APPLICATION_JSON)
                 .get(User.class);
 
         return user;
     }
 
-    public List<User> getUsers(){
-        List<User> userList=client
-                .target(URL+"user/list")
+    public List<User> getUsers() {
+        List<User> userList = client
+                .target(URL + Constants.URL_USER_LIST)
                 .request(MediaType.APPLICATION_JSON)
                 .get(List.class);
         return userList;
     }
 
-    public Status addUser(User user){
+    public Status addUser(User user) {
 
-        Status status=client
-                .target(URL+"create")
+        Status status = client
+                .target(URL + Constants.URL_CREATE_USER)
                 .request(MediaType.APPLICATION_JSON)
                 .accept(MediaType.TEXT_PLAIN)
-                .post(Entity.json(user),Status.class);
+                .post(Entity.json(user), Status.class);
 
         return status;
     }
 
-    public Status deleteUser(long id){
-        Status status=client
-                .target(URL+"delete/"+id)
+    public Status deleteUser(long id) {
+        Status status = client
+                .target(URL + Constants.URL_DELETE_USER + id)
                 .request(MediaType.TEXT_PLAIN).get(Status.class);
 
         return status;
---
+++ src/main/java/utils/Constants.java
@@ -0,0 +1,12 @@
+package utils;
+
+/**
+ * Created by vijayn on 8/5/2017.
+ */
+public interface Constants {
+     String URL = "http://localhost:8080/";
+     String URL_USER = "user/";
+     String URL_USER_LIST = "user/list";
+     String URL_CREATE_USER = "create";
+     String URL_DELETE_USER = "delete/";
+}
